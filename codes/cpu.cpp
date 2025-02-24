86603921f56d1efe20462a0afdd21901390704f6
Yoogi <yoogikov@gmail.com>
2025-02-24 19:54:44 +0530

    Added input and display functionality for graph

---
+++ codes/cpu.cpp
@@ -0,0 +1,6 @@
+#include "graph.cpp"
+
+int main(int argc, char *argv[]){
+    graph * G = input(argv[1]);
+    G->display();
+}
---
+++ codes/graph.cpp
@@ -0,0 +1,50 @@
+#include <fstream>
+#include <iostream>
+
+#ifdef __CUDACC__
+#define CUDA_ __host__ __device__
+#else
+#define CUDA_
+#endif 
+
+
+class graph{
+public:
+    CUDA_ int n_v, n_e;
+    CUDA_ int * offset;
+    CUDA_ int * edges;
+    CUDA_ int * weights;
+    CUDA_ void display(){
+        for(int i=0;i<n_v+1;i++)std::cout << offset[i] << " ";std::cout << std::endl;
+        for(int i=0;i<n_e;i++)std::cout << edges[i] << " ";std::cout << std::endl;
+        for(int i=0;i<n_e;i++)std::cout << weights[i] << " ";std::cout << std::endl;
+
+    }
+};
+
+struct graph * input(char * inp){
+    std::fstream fin;
+    fin.open(inp, std::ios::in);
+    int n_v, n_e;
+    fin >> n_v >> n_e;
+    std::cout << n_v << " " << n_e << std::endl;
+    
+    graph * G = new graph();
+    G->n_v = n_v;
+    G->n_e = n_e;
+    G->offset = (int *)malloc(sizeof(int)*(n_v+1));
+    G->edges = (int *)malloc(sizeof(int)*n_e);
+    G->weights = (int *)malloc(sizeof(int)*n_e);
+
+    for(int i=0;i<n_v+1;i++){
+        fin >> G->offset[i];
+    }
+    for(int i=0;i<n_e;i++){
+        fin >> G->edges[i];
+    }
+    for(int i=0;i<n_e;i++){
+        fin >> G->weights[i];
+    }
+
+    return G;
+};
